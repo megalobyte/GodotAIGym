a7fd2bd856b2dc55a499d96f292498147555e807
Greg Basile <greg.basile@yahoo.com>
2021-05-01 16:07:50 -0700

    cleanup. Install with mono

--- setup.py
+++ setup.py
@@ -6,48 +6,67 @@ from urllib import request
 from zipfile import ZipFile
 from pathlib import Path
 
-GODOT_PATH = os.environ["GODOT_PATH"]
+GODOT_PATH = "/home/greg/workspace/repos/godot-git/godot"
+OPTIONS = dict(platform='x11', tools='yes', target='release_debug', bits=64, module_mono_enabled='yes', use_llvm='yes', use_lld='yes')
 
 def download_unpack(rewrite=False):
-	# url = 'https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.7.0.zip'
-	url = 'https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.7.0%2Bcpu.zip'
-	if (not Path('libtorch.zip').exists()) or rewrite:
-		print('Downloading libtorch')
-		filedata = request.urlopen(url)
-		datatowrite = filedata.read()
-		with open('libtorch.zip', 'wb') as f:
-			f.write(datatowrite)
+        # url = 'https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.7.0.zip'
+        url = 'https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.7.0%2Bcpu.zip'
+        if (not Path('libtorch.zip').exists()) or rewrite:
+                print('Downloading libtorch')
+                filedata = request.urlopen(url)
+                datatowrite = filedata.read()
+                with open('libtorch.zip', 'wb') as f:
+                        f.write(datatowrite)
 
-	if (not Path('GodotModule/libtorch').exists()) or rewrite:
-		print('Extracting libtorch')
-		with ZipFile('libtorch.zip', 'r') as zipObj:
-   			zipObj.extractall(path='GodotModule')
-	
+        if (not Path('GodotModule/libtorch').exists()) or rewrite:
+                print('Extracting libtorch')
+                with ZipFile('libtorch.zip', 'r') as zipObj:
+                        zipObj.extractall(path='GodotModule')
+        
 
-def compile_godot(godot_root, platform='x11', tools='yes', target='release_debug', bits=64):
-	current_path = os.getcwd()
-	os.chdir(godot_root)
-	os.system(f"scons platform={platform} tools={tools} target={target} bits={bits}")
-	os.chdir(current_path)
+def compile_godot(godot_root, **kwargs):
+        OPTIONS.update(**kwargs)
+        current_path = os.getcwd()
+        os.chdir(godot_root)
+        arguments = " ".join([f"{k}={i}" for k, i in OPTIONS.items()])
+        command = f"scons -j$(nproc) {arguments}"
+        print(command)
+        os.system(command)
+        os.chdir(current_path)
 
 def install_module(godot_root, rewrite=False):
-	module_dir = os.path.join(godot_root, 'modules/GodotSharedMemory')
-	if (not os.path.exists(module_dir)):
-		copytree('GodotModule', module_dir)
-	elif rewrite:
-		rmtree(module_dir)
-		copytree('GodotModule', module_dir)
-	
+        module_dir = os.path.join(godot_root, 'modules/GodotSharedMemory')
+        if (not os.path.exists(module_dir)):
+                copytree('GodotModule', module_dir)
+        elif rewrite:
+                rmtree(module_dir)
+                copytree('GodotModule', module_dir)
+        
 def install_python_module():
-	current_path = os.getcwd()
-	os.chdir('PythonModule')
-	os.system('python setup.py install')
-	os.chdir(current_path)
+        current_path = os.getcwd()
+        os.chdir('PythonModule')
+        os.system('python3 -m pip install -e .')
+        os.chdir(current_path)
+
+def make_glue():
+        import glob
+        current_path = os.getcwd()
+        os.chdir(GODOT_PATH)
+        godot_exec = glob.glob(f'./bin/godot.{OPTIONS["platform"]}.*.{OPTIONS["bits"]}.*.mono')
+        assert len(godot_exec) == 1, print(godot_exec)
+        godot_exec = godot_exec[0]
+        os.system(f'{godot_exec} --generate-mono-glue modules/mono/glue')
+        os.chdir(current_path)
+    
 
 if __name__=='__main__':
-	download_unpack(rewrite=False)
-	install_module(godot_root=GODOT_PATH, rewrite=True)
-	install_python_module()
-	compile_godot(godot_root=GODOT_PATH, platform='x11', tools='yes', target='release_debug', bits=64)
-	compile_godot(godot_root=GODOT_PATH, platform='x11', tools='no', target='release_debug', bits=64)
-	compile_godot(godot_root=GODOT_PATH, platform='server', tools='no', target='release_debug', bits=64)
+        download_unpack(rewrite=False)
+        install_module(godot_root=GODOT_PATH, rewrite=True)
+        install_python_module()
+        # build compile mono
+        compile_godot(godot_root=GODOT_PATH, mono_glue='no')
+        make_glue()
+        compile_godot(godot_root=GODOT_PATH, mono_glue='yes')
+        compile_godot(godot_root=GODOT_PATH, tools='no')
+        compile_godot(godot_root=GODOT_PATH, platform='server')
